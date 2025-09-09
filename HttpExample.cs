7d13c1ae315b09019144fd62d7bc0f074ee16bed
tyduc45 <3381274436@qq.com>
2025-09-09 18:36:25 +1000

    添加项目文件。

---
+++ HttpExample.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace swe40006c;
+
+public class HttpExample
+{
+    private readonly ILogger<HttpExample> _logger;
+
+    public HttpExample(ILogger<HttpExample> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("HttpExample")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
